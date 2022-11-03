a5d438881eb8cd3cdda2dc58f31f858ddf232e5f
Leandro Bastos <17bastos@gmail.com>
2022-11-03 19:55:45 -0300

    metade da tela de cadastro

--- lib/src/auth/Sing_up_screen.dart
+++ lib/src/auth/Sing_up_screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: file_names
+
+import 'package:appquitanda/src/auth/components/customs_text_fields.dart';
 import 'package:appquitanda/src/config/custom_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -6,23 +9,95 @@ class SingUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: const [
-          Expanded(
-            child: Center(
-              child: Text(
-                "cadastro",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        "cadastro",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //formulario
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        //email
+                        const CustomTextFormField(
+                          icon: Icons.email,
+                          label: 'Email',
+                        ),
+                        //senha
+                        const CustomTextFormField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        //nome
+                        const CustomTextFormField(
+                          icon: Icons.person,
+                          label: 'Nome',
+                        ),
+                        //celular
+                        const CustomTextFormField(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                        ),
+                        //cpf
+                        const CustomTextFormField(
+                          icon: Icons.document_scanner,
+                          label: 'CPF',
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18))),
+                            onPressed: () {},
+                            child: const Text(
+                              'Cadastrar usuário',
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ),
+              Container(
+                color: Colors.red,
+                height: 50,
+                width: 50,
+              )
+            ],
           ),
-          //formulario
-        ],
+        ),
       ),
     );
   }
