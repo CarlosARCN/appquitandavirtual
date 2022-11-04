521794eef893bbc2fc8a487919d50340343859e6
Leandro Bastos <17bastos@gmail.com>
2022-11-04 15:54:08 -0300

    HomeTab

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   const BaseScreen({super.key});
 
   @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int currentindex = 0;
+  final pageController = PageController();
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //pages
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        children: [
+          //page1
+          Container(
+            color: Colors.red,
+          ),
+          //page2
+          Container(
+            color: Colors.blue,
+          ),
+          //page3
+          Container(
+            color: Colors.green,
+          ),
+          //page4
+          Container(
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      //menu de baixo
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentindex,
+        onTap: (index) {
+          setState(() {
+            currentindex = index;
+            pageController.jumpToPage(index);
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          //icones do menu
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Carrinho',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Pedidos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/src/home/Home_tab.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //appbar
+      appBar: AppBar(
+        title: const Text.rich(TextSpan(children: [])),
+      ),
+
+      //pesquisa
+
+      //categorias
+
+      //grid
+    );
+  }
+}
