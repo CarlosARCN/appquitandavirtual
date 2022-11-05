2dcbe1db8bd84de1f60b4eba581a399a4e2df21d
Leandro Bastos <17bastos@gmail.com>
2022-11-05 00:51:41 -0300

    primeiro do dia ou ultimo da noite?!

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,9 @@
+import 'package:appquitanda/src/models/Iten_model.dart';
+
+ItemModel apple = ItemModel(
+  description: 'a melhor apple',
+  imgUrl: 'assets/fruits/apple.png',
+  itemName: 'Maça',
+  price: 5.5,
+  unit: 'kg',
+);
