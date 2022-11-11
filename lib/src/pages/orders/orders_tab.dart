a8f76525f9abb6bdffd757e0f30d1bfc2649f426
Leandro Bastos <17bastos@gmail.com>
2022-11-11 11:20:09 -0300

    flha critica detectada na integracao com sistema nevas

--- lib/src/pages/orders/orders_tab.dart
+++ lib/src/pages/orders/orders_tab.dart
@@ -2,6 +2,7 @@
 
 import 'package:appquitanda/src/config/custom_colors.dart';
 import 'package:appquitanda/src/config/app_data.dart' as appData;
+import 'package:appquitanda/src/pages/orders/components/Order_Tile.dart';
 import 'package:flutter/material.dart';
 
 class OrdersTab extends StatelessWidget {
@@ -21,12 +22,9 @@ class OrdersTab extends StatelessWidget {
       body: ListView.separated(
         padding: const EdgeInsets.all(16),
         physics: const BouncingScrollPhysics(),
-        itemBuilder: ((context, index) {
-          return Text(appData.orders[index].id);
-        }),
-        separatorBuilder: (_, index) => const SizedBox(
-          height: 10,
-        ),
+        itemBuilder: ((context, index) =>
+            OrderTile(order: appData.orders[index])),
+        separatorBuilder: (_, index) => const SizedBox(height: 10),
         itemCount: appData.orders.length,
       ),
     );
